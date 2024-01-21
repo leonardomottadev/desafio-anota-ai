3a46f60122cb98549ceeafca2e2a136615ae02ff
Leonardo Motta <leonardomotta@ucl.br>
2024-01-21 10:23:44 -0300

    fix ProductRepository

--- src/main/java/com/leomottadev/desafioanotaai/repositories/ProductRepository.java
+++ src/main/java/com/leomottadev/desafioanotaai/repositories/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.leomottadev.desafioanotaai.repositories;
 
 import com.leomottadev.desafioanotaai.domain.category.Category;
+import com.leomottadev.desafioanotaai.domain.product.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Category, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 
 }
