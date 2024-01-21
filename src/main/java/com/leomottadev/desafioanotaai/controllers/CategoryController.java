06d4ebe0a90cb05a8717525852548cd5c84c83ec
Leonardo Motta <leonardomotta@ucl.br>
2024-01-21 12:12:17 -0300

    add controllers

---
+++ src/main/java/com/leomottadev/desafioanotaai/controllers/CategoryController.java
@@ -0,0 +1,41 @@
+package com.leomottadev.desafioanotaai.controllers;
+
+import com.leomottadev.desafioanotaai.domain.category.Category;
+import com.leomottadev.desafioanotaai.domain.category.CategoryDTO;
+import com.leomottadev.desafioanotaai.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService service;
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody CategoryDTO categoryData) {
+        Category newCategory = this.service.insert(categoryData);
+        return ResponseEntity.ok(newCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll() {
+        List<Category> categories = this.service.getAll();
+        return ResponseEntity.ok(categories);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> update(@PathVariable("id") String id, @RequestBody CategoryDTO categoryData) {
+        Category newCategory = this.service.update(id, categoryData);
+        return ResponseEntity.ok().body(newCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> delete(@PathVariable("id") String id) {
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/leomottadev/desafioanotaai/controllers/ProductController.java
@@ -0,0 +1,41 @@
+package com.leomottadev.desafioanotaai.controllers;
+
+import com.leomottadev.desafioanotaai.domain.product.Product;
+import com.leomottadev.desafioanotaai.domain.product.ProductDTO;
+import com.leomottadev.desafioanotaai.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody ProductDTO productData) {
+        Product newProduct = this.service.insert(productData);
+        return ResponseEntity.ok(newProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAll() {
+        List<Product> products = this.service.getAll();
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable("id") String id, @RequestBody ProductDTO productData) {
+        Product newProduct = this.service.update(id, productData);
+        return ResponseEntity.ok().body(newProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> delete(@PathVariable("id") String id) {
+        this.service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
